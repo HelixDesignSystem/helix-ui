43bb7c7281a5b95133e8a466d83f583345bdde11
Droogans <droogans@gmail.com>
2017-10-17 14:38:01 -0400

    docs(visreg): Explain the example config better

--- test/bin/visreg.example.config.ts
+++ test/bin/visreg.example.config.ts
@@ -1,3 +1,7 @@
+/*
+ * This is simply exported so that typescript can give you hints
+ * about what your config has in it.
+ */
 export interface IConfig {
     githubEmail: string,
     githubName: string,
@@ -5,9 +9,16 @@ export interface IConfig {
     screenshotsDirectory: string,
 };
 
+/*
+ * Configuration object for the local visreg service.
+ * Needed to associate visreg job runs to your github profile.
+ * Repo name can stay the same, but you should update the other
+ * values and then finally, copy these changes over to the (ignored)
+ * `visreg.config.ts` file.
+ */
 export var config: IConfig = {
-    "githubEmail": "andrew.yurisich@rackspace.com",
-    "githubName": "andr6283",
+    "githubEmail": "your.email@rackspace.com",
+    "githubName": "name1234",
     "repo": "catalog-helix-ui-0",
     "screenshotsDirectory": "screenshots",
 }
