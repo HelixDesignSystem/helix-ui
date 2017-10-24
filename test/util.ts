0af6a1fb9d8ef7164cd6e94aa60613a5fd8bc703
Droogans <droogans@gmail.com>
2017-10-24 14:44:54 -0400

    refact(visreg): Share one set of tests for both browsers

--- test/util.ts
+++ test/util.ts
@@ -1,7 +1,11 @@
 /*
  * Helper functions for the tests in `index.ts`.
  */
+import * as _ from "lodash";
 import {By, ISize, ThenableWebDriver, WebDriver, WebElementPromise} from "selenium-webdriver";
+
+import {$, snap, Snappit, IConfig} from "snappit-visual-regression";
+
 export async function setViewportSize (
     driver: ThenableWebDriver,
     size: ISize,
@@ -31,6 +35,56 @@ export function $x(
 }
 
 // "page object"
-export var $ = {
+export var selectors = {
     nav: ".hxApp__nav",
+}
+
+export function visregSuite(browserName: string) {
+    return () => {
+        let snappit: Snappit;
+        let driver: any;
+
+        describe(browserName, () => {
+            beforeAll(async () => {
+                const config: IConfig = {
+                    browser: browserName,
+                    screenshotsDir: "screenshots",
+                    logException: [
+                        "MISMATCH",
+                        "NO_BASELINE",
+                        "SIZE_DIFFERENCE",
+                    ],
+                    threshold: 0.1,
+                    useDirect: true,
+                };
+
+                snappit = new Snappit(config);
+                driver = await snappit.start();
+                await setViewportSize(driver, { width: 1366, height: 768 });
+                driver.get("http://localhost:3000/");
+            });
+
+            it("full-screen", async () => {
+                await snap("{browserName}/index");
+            });
+
+            it("nav", async () => {
+                await snap("{browserName}/nav", $(selectors.nav));
+            });
+
+            it("guides", async () => {
+                await $x(driver, "//nav/hx-reveal//header", "Guides").click();
+                await snap("{browserName}/nav/guides", $(selectors.nav));
+            });
+
+            it("components", async () => {
+                await $x(driver, "//nav/hx-reveal//header", "Components").click();
+                await snap("{browserName}/nav/componenets", $(selectors.nav));
+            });
+
+            afterAll(async () => {
+                await snappit.stop();
+            });
+        });
+    }
 }
