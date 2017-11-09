dfe56c6c2fa0092cb52e76bff819a5afa0223ce3
Droogans <droogans@gmail.com>
2017-11-09 15:56:48 -0500

    feat(server): Hook testing into watch tasks

--- bin/start.js
+++ bin/start.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 'use strict';
 
+const fs = require('fs');
+const exec = require('child_process').exec;
+
 const Build = require('./build');
 const CONFIG = require('../_config');
 const Clean = require('./clean');
@@ -16,17 +19,46 @@ Build.buildSync();
 const serverRoutes = {}
 serverRoutes[CONFIG.site.baseHref] = CONFIG.publicDir;
 
+browserSync.emitter.on('init', () => {
+    console.log('Starting a selenium webdriver instance...');
+    exec('yarn run webdriver:start', { cwd: CONFIG.testDir }); // don't log anything to the dev server
+});
+
 browserSync.init({
     files: [ // (See 'watchEvents')
         // Reload browser if any file in public directory changes
         `${CONFIG.publicDir}/**/*`,
+
         // Rebuild if anything changes in source directory
         {
             match: [
                 `${CONFIG.sourceDir}/**/*`
             ],
-            fn: (evt, file) => {
+            fn: () => {
                 Build.buildSync();
+
+                /**
+                 * Always ensure that this task runs last, as it needs to wait for all
+                 * synchronous build events to complete so that browserSync can finish
+                 * and reload the page (and therefore, allow selenium to connnect).
+                 */
+                const tests = exec('yarn run test:regression -- -u', { cwd: CONFIG.testDir });
+                // this *is* asynchronous, see comment above
+                tests.stdout.pipe(process.stdout);
+                tests.stderr.pipe(process.stderr);
+            }
+        },
+
+        // Re-transpile test files
+        {
+            match: [
+                `${CONFIG.testDir}/**/*.ts`,
+                `!${CONFIG.testDir}/built/**/*`
+            ],
+            fn: () => {
+                const tsc = exec('yarn run build', { cwd: CONFIG.testDir });
+                tsc.stdout.pipe(process.stdout);
+                tsc.stderr.pipe(process.stderr);
             }
         },
     ],
