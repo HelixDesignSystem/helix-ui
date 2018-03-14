76dc3789fb7989edc373adb1a7b634d626c7a638
Ryan A. Johnson <ryan.johnson@rackspace.com>
2018-03-14 14:07:19 -0500

    docs(versions): Add min version to relevant documentation
    * Add pipeline logic to generate JSON files for VueJS consumption in docs
    * Add filtering feature to Icons component
    * Update Icon list layout

---
+++ docs/components/icons/icon-demo.js
@@ -0,0 +1,43 @@
+if (document.getElementById('vue-iconDemo')) {
+    const Icons = {
+        fetchAll: function () {
+            return $.ajax({
+                async: false,
+                dataType: 'json',
+                url: '/data/icons.json',
+            });
+        },
+    };//Icons
+
+    new Vue({
+        el: '#vue-iconDemo',
+        data: {
+            icons: [],
+            filter: '',
+        },
+        beforeMount: function () {
+            Icons.fetchAll().then((data) => {
+                this.icons = data;
+            });
+        },
+        methods: {
+            onFilterUpdate: function (evt) {
+                this.filter = evt.currentTarget.value || '';
+            },
+        },
+        computed: {
+            hasFilter: function () {
+                return this.filter !== '';
+            },
+            filteredIcons: function () {
+                let filtered = this.icons;
+                if (this.hasFilter) {
+                    filtered = this.icons.filter((icon) => {
+                        return icon.name.indexOf(this.filter) >= 0;
+                    });
+                }
+                return filtered;
+            },
+        },
+    });
+}
