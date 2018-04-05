a5e48b4148b654a2199102515400d97466c14cf0
Ryan A. Johnson <ryan.johnson@rackspace.com>
2018-04-04 22:00:21 -0500

    feat(Tabs): add events
    * (HXTabset): Emit "tabchange" when current tab changes
    * (HXTabPanel): Emit "open" and "close" events
    
    SURF-988

--- src/helix-ui/elements/HXTabpanelElement.js
+++ src/helix-ui/elements/HXTabpanelElement.js
@@ -1,19 +1,40 @@
-import { HXRevealElement } from './HXRevealElement';
+import { HXElement } from './HXElement';
 
-export class HXTabpanelElement extends HXRevealElement {
+export class HXTabpanelElement extends HXElement {
     static get is () {
         return 'hx-tabpanel';
     }
 
-    connectedCallback () {
-        super.connectedCallback();
-        this.$defaultAttribute('role', 'tabpanel');
+    static get observedAttributes () {
+        return [ 'open' ];
     }
 
-    // because we are inheriting HXReveal, the only attribute we are watching
-    // is "open"
+    connectedCallback () {
+        this.$defaultAttribute('role', 'tabpanel');
+        this.$upgradeProperty('open');
+        this.setAttribute('aria-expanded', this.open);
+    }
+
     attributeChangedCallback (attr, oldVal, newVal) {
-        super.attributeChangedCallback(arguments);
-        this.setAttribute('tabindex', (newVal !== null) ? 0 : -1);
+        let isOpen = (newVal !== null);
+
+        this.setAttribute('aria-expanded', isOpen);
+        this.setAttribute('tabindex', (isOpen ? 0 : -1));
+
+        if (newVal !== oldVal) {
+            this.$emit(isOpen ? 'open' : 'close');
+        }
+    }
+
+    get open () {
+        return this.hasAttribute('open');
+    }
+
+    set open (value) {
+        if (value) {
+            this.setAttribute('open', '');
+        } else {
+            this.removeAttribute('open');
+        }
     }
 }//HXTabpanelElement
