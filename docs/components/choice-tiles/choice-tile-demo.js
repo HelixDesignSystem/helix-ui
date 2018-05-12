aa5f49d435a86f043b21050e0527a1b712158bb5
Ryan A. Johnson <ryan.johnson@rackspace.com>
2018-05-11 21:52:22 -0500

    feat(optiontile): wip

--- docs/components/choice-tiles/choice-tile-demo.js
+++ docs/components/choice-tiles/choice-tile-demo.js
@@ -1,23 +1,23 @@
 import Util from '../../_util';
 
 if (document.getElementById('vue-choiceDemo')) {
+    const SIZES = [
+        { value: 'hxSm', label: 'Small' },
+        { value: '', label: 'Default' },
+        { value: 'hxLg', label: 'Large' },
+    ];
+
     new Vue({
         el: '#vue-choiceDemo',
         data: {
-            size: {
-                label: 'Default',
-                value: '',
-            },
-            sizes: [
-                { value: 'hxSm', label: 'Small' },
-                { value: '', label: 'Default' },
-                { value: 'hxLg', label: 'Large' },
-            ],
             description: 'A couple of descriptive lines or a small bit of help text.',
             isChecked: false,
             isDisabled: false,
             isInvalid: false,
             isSubdued: false,
+            size: SIZES[1],
+            sizes: SIZES,
+            title: 'Title Here',
         },
         methods: {
             onClick: function (evt) {
@@ -38,22 +38,15 @@ if (document.getElementById('vue-choiceDemo')) {
             },
             snippet: function () {
                 return Util.snippet(`
-                  <label class="hxChoice">
+                  <label class="hxOption">
                     <input type="radio"
                         ${this.isChecked ? 'checked' : ''}
                         ${this.isDisabled ? 'disabled' : ''}
                         ${this.isInvalid ? 'invalid' : ''}>
-                    <hx-tile ${this.hasClasses ? `class="${this.tileClasses}"` : ''}>
-                      <hx-icon type="checkmark"></hx-icon>
-                      <div class="hx-tile-icon">
-                        <hx-icon type="account"></hx-icon>
-                      </div>    
-                      <hx-tile-title>
-                      Title here
-                      </hx-tile-title>
-                      <hx-tile-description>
-                        ${this.description}
-                      </hx-tile-description>
+                    <hx-option-tile ${this.hasClasses ? `class="${this.tileClasses}"` : ''}>
+                      <hx-icon type="account"></hx-icon>
+                      <header>${this.title}</header>
+                      <p>${this.description}</p>
                     </hx-tile>
                   </label>
                 `);
