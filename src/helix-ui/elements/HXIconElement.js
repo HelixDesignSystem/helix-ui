b24e09fa347a02949b78bf0e2954d9a7638667ed
Ryan A. Johnson <ryan.johnson@rackspace.com>
2018-08-13 18:01:46 -0500

    fix(HXIcon): correct issues with HXIconElement
    * add documentation around `<hx-icon>` being an inline element
    * add Shadow Markup for HXIconElement
    * add Shadow Styles for HXIconElement
    * remove unnecessary @imports for hx-icon styles
    * remove unnecessary `fill-rule` attributes from SVG files
    * remove HXIconElement.icons property (see #309)
    * update regression snapshots
    
    Closes:
    * SURF-1224 (inconsistent button heights w/ icons)
    * SURF-1260 (multiple rendering of hx-icon)
    * SURF-1293 (JSX: `<hx-error>`)
    * SURF-1272 (JSX: Icons)
    * SURF-1114 (focus style of "X" in `<hx-search>`)

--- src/helix-ui/elements/HXIconElement.js
+++ src/helix-ui/elements/HXIconElement.js
@@ -1,5 +1,9 @@
 import { HXElement } from './HXElement';
 import ICONS from '../icons';
+import shadowStyles from './HXIconElement.less';
+import shadowMarkup from './HXIconElement.html';
+
+const DIV = document.createElement('div');
 
 /**
  * Defines behavior for the `<hx-icon>` element.
@@ -12,55 +16,85 @@ export class HXIconElement extends HXElement {
         return 'hx-icon';
     }
 
-    static get icons () {
-        return ICONS;
-    }
-
-    $onCreate (type) {
-        if (type) {
-            this.type = type;
-        }
+    static get template () {
+        return `<style>${shadowStyles}</style>${shadowMarkup}`;
     }
 
     $onConnect () {
         this.$upgradeProperty('type');
         this.$defaultAttribute('aria-hidden', true);
-        this._render();
     }
 
     static get $observedAttributes () {
         return [ 'type' ];
     }
 
-    $onAttributeChange (attr) {
+    $onAttributeChange (attr, oldVal, newVal) {
         if (attr === 'type') {
-            this._render();
+            this._attrTypeChange(oldVal, newVal);
         }
     }
 
+    /** @type {string} */
     get type () {
         return this.getAttribute('type');
     }
 
+    /** @type {string} */
     set type (newVal) {
         this.setAttribute('type', newVal);
     }
 
-    _render () {
-        // erase previously injected markup
-        this.innerHTML = '';
-
-        if (this.type in ICONS) {
-            // create surrogate DIV to add raw SVG markup
-            const tmpDiv = document.createElement('div');
-            tmpDiv.innerHTML = ICONS[this.type];
-            // grab SVG from surrogate DIV
-            const svg = tmpDiv.firstElementChild;
-            // Prevent IE/Edge from adding SVG to focus order
-            svg.setAttribute('focusable', 'false');
-
-            // inject SVG into Light DOM
-            this.appendChild(svg);
+    /**
+     * This function is dependent on all SVG markup containing
+     * a SINGLE `<path>` element.
+     *
+     * This is expected SVG markup.
+     * ```html
+     * <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+     *   <path d="..." />
+     * </svg>
+     * ```
+     *
+     * The following markup is not supported, because there are two `<path>`
+     * elements in the SVG document.
+     * ```html
+     * <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+     *   <path d="..." />
+     *   <path d="..." />
+     * </svg>
+     * ```
+     *
+     * The following markup isn't supported, but it may still work.
+     * However, the file contains unnecessary markup, which will directly
+     * affect the file size of generated JavaScript assets.
+     * ```html
+     * <svg
+     *   xmlns="http://www.w3.org/2000/svg"
+     *   xmlns:xlink="http://www.w3.org/1999/xlink"
+     *   viewBox="0 0 16 16"
+     * >
+     *   <defs>
+     *     <path id="icon-a" d="..."/>
+     *   </defs>
+     *   <g fill="none" fill-rule="evenodd">
+     *     <use fill="#000" xlink:href="#account-a"/>
+     *   </g>
+     * </svg>
+     * ```
+     */
+    _attrTypeChange (oldVal, newVal) {
+        let d = '';
+        if (newVal in ICONS) {
+            DIV.innerHTML = ICONS[newVal];
+            let path = DIV.querySelector('path');
+            d = (path ? path.getAttribute('d') : '');
         }
-    }//_render()
+        this._svgPath.setAttribute('d', d);
+    }
+
+    /** @private */
+    get _svgPath () {
+        return this.shadowRoot.getElementById('hxPath');
+    }
 }
