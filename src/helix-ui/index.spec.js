6bdee7e70ea3e3a2102a734bad1c14197dad96a0
Shaleen Agarwal <shaleen76@gmail.com>
2018-08-16 15:45:18 -0500

    Our first unit test suite, for initalize using chai, mocha, sinon, sinon-chai

--- src/helix-ui/index.spec.js
+++ src/helix-ui/index.spec.js
@@ -1,15 +1,18 @@
-//import * as HelixUI from 'helix-ui';
-
-const HelixUI = require('helix-ui');
-const jsdom = require("jsdom");
+const jsdom = require('jsdom');
 const sinon = require('sinon');
+const chai = require('chai');
+const sinonChai = require("sinon-chai");
+const expect = chai.expect;
+chai.use(sinonChai);
+
 global.window = new jsdom.JSDOM().window;
 global.document = window.document;
 global.HTMLElement = window.HTMLElement;
 
+const HelixUI = require('helix-ui');
 describe('helix-ui', function() {
     it('it should define initialize', () => {
-        let initilaizeSpy = sinon.spy(HelixUI.initialize);
-        expect(initilaizeSpy).to.have.been.calledOnce;
+        let initializeSpy = sinon.spy(HelixUI.initialize);
+        expect(initializeSpy).not.to.have.been.called;
     });
 });
