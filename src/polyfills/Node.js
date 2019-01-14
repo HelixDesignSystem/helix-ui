047f92d8e37503e02308cad6ec51a14c320bfc69
Ryan A. Johnson <ryan.johnson@rackspace.com>
2019-01-14 12:25:35 -0600

    revert(polyfills): remove unnecessary isConnected polyfill

--- src/polyfills/Node.js
+++
@@ -1,9 +0,0 @@
-var proto = Node.prototype;
-if (!proto.hasOwnProperty('isConnected')) {
-    Object.defineProperty(proto, 'isConnected', {
-        enumerable: true,
-        get: function () {
-            return (this.getRootNode().nodeType === Node.DOCUMENT_NODE);
-        },
-    });
-}
