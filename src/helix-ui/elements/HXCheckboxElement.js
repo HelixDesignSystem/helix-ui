af2223b2b9c732ccdd643a6a176efdb244c3b353
Ryan A. Johnson <ryan.johnson@rackspace.com>
2019-02-21 17:09:19 -0600

    refactor(HXCheckboxElement): refactor <hx-checkbox> logic for unwrapping

--- src/helix-ui/elements/HXCheckboxElement.js
+++ src/helix-ui/elements/HXCheckboxElement.js
@@ -3,123 +3,57 @@ import shadowMarkup from './HXCheckboxElement.html';
 import shadowStyles from './HXCheckboxElement.less';
 
 /**
- * Fires when the element's `checked` state changes
- *
- * @event Checkbox:change
- * @since 0.1.8
- * @type {CustomEvent}
- */
-
-/**
  * Defines behavior for the `<hx-checkbox>` element.
  *
- * @emits Checkbox:change
  * @extends HXElement
  * @hideconstructor
- * @since 0.1.8
  */
 export class HXCheckboxElement extends HXElement {
+    /** @override */
     static get is () {
         return 'hx-checkbox';
     }
 
+    /** @override */
     static get template () {
         return `<style>${shadowStyles}</style>${shadowMarkup}`;
     }
 
-    $onCreate () {
-        this._onChange = this._onChange.bind(this);
-    }
-
+    /** @override */
     $onConnect () {
-        this.$upgradeProperty('checked');
-        this.$upgradeProperty('disabled');
-        this.$upgradeProperty('indeterminate');
-        this._chkInput.addEventListener('change', this._onChange);
+        this.addEventListener('click', this._onClick);
     }
 
+    /** @override */
     $onDisconnect () {
-        this._chkInput.removeEventListener('change', this._onChange);
-    }
-
-    static get $observedAttributes () {
-        return [
-            'checked',
-            'disabled',
-            'indeterminate',
-        ];
-    }
-
-    $onAttributeChange (attr, oldVal, newVal) {
-        const hasValue = (newVal !== null);
-        switch (attr) {
-            case 'indeterminate':
-                this._chkInput.indeterminate = hasValue;
-                break;
-            case 'checked':
-                if (this._chkInput.checked !== hasValue) {
-                    this._chkInput.checked = hasValue;
-                }
-                break;
-            case 'disabled':
-                this._chkInput.disabled = hasValue;
-                break;
-        }
+        this.removeEventListener('click', this._onClick);
     }
 
     /**
-     * @default false
-     * @type {Boolean}
+     * @readonly
+     * @type {HTMLElement}
      */
-    get checked () {
-        return this.hasAttribute('checked');
-    }
-    set checked (value) {
-        if (value) {
-            this.setAttribute('checked', '');
-        } else {
-            this.removeAttribute('checked');
-        }
+    get controlElement () {
+        return this.getRootNode().querySelector(`[id="${this.htmlFor}"]`);
     }
 
     /**
-     * Indicates if the state of the element cannot be determined.
+     * ID of associated checkbox control.
      *
-     * @default false
-     * @type {Boolean}
+     * @type {string}
      */
-    get indeterminate () {
-        return this.hasAttribute('indeterminate');
+    get htmlFor () {
+        return this.getAttribute('for') || '';
     }
-    set indeterminate (value) {
-        if (value) {
-            this.setAttribute('indeterminate', '');
-        } else {
-            this.removeAttribute('indeterminate');
+    set htmlFor (value) {
+        this.setAttribute('for', value);
+    }
+
+    /** @private */
+    _onClick () {
+        let ctrl = this.controlElement;
+        if (ctrl) {
+            ctrl.click();
         }
-    }
-
-    /**
-     * Pass-through function to native input.
-     */
-    click () {
-        this._chkInput.click();
-    }
-
-    /** @private */
-    _onChange (evt) {
-        // Update internal state
-        this.checked = evt.target.checked;
-
-        // Prevent 'change' listeners from firing twice in polyfilled browsers.
-        evt.stopImmediatePropagation();
-
-        // Emit a new 'change' event from the custom element
-        this.$emit('change');
-    }
-
-    /** @private */
-    get _chkInput () {
-        return this.shadowRoot.getElementById('hxNativeControl');
     }
 }
