61f9b1864ffd0d1cf6c539d4e91472923760559a
Ryan A. Johnson <ryan.johnson@rackspace.com>
2019-04-15 19:39:38 -0500

    fix(DropdownSelect): fix and refactor Dropdown Select
    - Fix focus styles
    - Add support for `<label>` within `<hx-select-control>`
    - Refactor CSS with overloaded mixins
    - Add extra whitespace to example demos to allow them to breathe.
    - Tweak documentation wording across a few components.

--- docs/components/dropdown-select/select-demo.js
+++ docs/components/dropdown-select/select-demo.js
@@ -4,8 +4,11 @@ if (document.getElementById('vue-selectDemo')) {
     new Vue({
         el: '#vue-selectDemo',
         data: {
+            hasAsterisk: false,
+            hasOptional: false,
             isDisabled: false,
             isRequired: false,
+            label: 'Choose an Option',
         },
         computed: {
             attrDisabled: function () {
@@ -14,17 +17,38 @@ if (document.getElementById('vue-selectDemo')) {
             attrRequired: function () {
                 return (this.isRequired ? 'required' : '');
             },
+            lblClasses: function () {
+                let classes = [];
+
+                if (this.hasAsterisk) {
+                    classes.push('hxRequired');
+                }
+
+                if (this.hasOptional) {
+                    classes.push('hxOptional');
+                }
+
+                let classNames = classes.join(' ');
+
+                return (classNames === '' ? '' : `class="${classNames}"`);
+            },
             snippet: function () {
                 return Util.snippet(`
                     <hx-select-control>
                       <select
+                        id="selDemo"
                         ${this.attrDisabled}
                         ${this.attrRequired}
                       >
-                        ...
+                        <!-- ... -->
                       </select>
-
                       <hx-select></hx-select>
+                      <label
+                        for="selDemo"
+                        ${this.lblClasses}
+                      >
+                        ${this.label}
+                      </label>
                     </hx-select-control>
                 `);
             },
