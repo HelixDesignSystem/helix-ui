7cbb4e3e8fc1f08935ff0a7b86cadb9fb0cfa04e
Lalith Kumar Karikelli <lalith.karikelli@rackspace.com>
2020-03-24 17:02:25 +0530

    fix(review comments changes ): surf1711

--- src/elements/hx-email-control/index.js
+++ src/elements/hx-email-control/index.js
@@ -1,28 +1,28 @@
 import { HXFormControlElement } from '../../interfaces/HXFormControlElement/index.js';
 
-/**
- * Defines behavior for the `<hx-text-control>` element.
- *
- * @extends HXFormControlElement
- * @hideconstructor
- * @since 0.16.0
- */
+///
+///Defines behavior for the `<hx-email-control>` element.
+///
+///@extends HXFormControlElement
+///@hideconstructor
+///@since 0.20.0
+///
 export class HXEmailControl extends HXFormControlElement {
-    /** @override */
-    static get is () {
+    // @override 
+    static get is() {
         return 'hx-email-control';
     }
 
-    /**
-     * Fetch the first text `<input>` descendant,
-     * whether implicit (`<input />`) or explicit
-     * (`<input type="text" />`).
-     *
-     * @override
-     * @readonly
-     * @type {?HTMLInputElement}
-     */
-    get controlElement () {
+    ///
+    /// Fetch the first `<input>` with 'type="email"' descendant,
+    /// whether implicit (`<input />`) or explicit
+    /// (`<input type="email" />`).
+    ///
+    /// @override
+    /// @readonly
+    /// @type {?HTMLInputElement}
+    ///
+    get controlElement() {
         return this.querySelector('input:not([type]), input[type="email"]');
     }
 }
