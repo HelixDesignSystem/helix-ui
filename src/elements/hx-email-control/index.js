17e533a45bd152cf0c488c084e21eb3b25198511
Lalith Kumar Karikelli <lalith.karikelli@rackspace.com>
2020-03-26 18:19:29 +0530

    fix(disable and enable for fields): surf1711

--- src/elements/hx-email-control/index.js
+++ src/elements/hx-email-control/index.js
@@ -7,22 +7,21 @@ import { HXFormControlElement } from '../../interfaces/HXFormControlElement/inde
  * @since 0.20.0
  */
 
-
 export class HXEmailControl extends HXFormControlElement {
     // @override 
     static get is () {
         return 'hx-email-control';
     }
 
-     /** 
-      *  Fetch the first `<input>` with 'type="email"' descendant,
-      * whether implicit (`<input />`) or explicit
-      * (`<input type="email" />`).
-      * 
-      * @override
-      * @readonly
-      * @type {?HTMLInputElement} 
-      */
+    /** 
+     * Fetch the first `<input>` with 'type="email"' descendant,
+     * whether implicit (`<input />`) or explicit
+     * (`<input type="email" />`).
+     * 
+     * @override
+     * @readonly
+     * @type {?HTMLInputElement} 
+     */
     get controlElement () {
         return this.querySelector('input:not([type]), input[type="email"]');
     }
