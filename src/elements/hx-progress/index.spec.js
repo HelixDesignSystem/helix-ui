336040503c258ce910220432ca6b88e877f53d5c
Lalith Kumar Karikelli <lalith.karikelli@rackspace.com>
2020-05-13 19:45:44 +0530

    fix(review changes): surf2017

--- src/elements/hx-progress/index.spec.js
+++ src/elements/hx-progress/index.spec.js
@@ -6,33 +6,56 @@ import { fixture, expect } from '@open-wc/testing';
  * @type HXProgressElement
  *
  */
-describe('<hx-progress> tests', () => {
-
+describe('<hx-progress> component tests', () => {
     const template = '<hx-progress>';
 
-    it('render the Shadow Root mode open', async () => {
-        const el = /** @type { HXProgressElement } */ await fixture(template);
-        const mode = el.shadowRoot.mode;
+    describe('instantiate element', () => {
+        it('should be instantiated with hx-defined attribute', async () => {
+            const component = /** @type {HXProgressElement} */ await fixture(template);
+            const attr = component.hasAttribute('hx-defined');
 
-        expect(mode).to.equal("open");
+            expect(attr).to.be.true;
+        });
+
+        it('should not be hidden', async () => {
+            const component = /** @type {HXProgressElement} */ await fixture(template);
+            const prop = component.hidden;
+
+            expect(prop).to.be.false;
+        });
+
+        it(`the rendered light DOM should NOT equal simple template ${template}`, async () => {
+            const component = /** @type {HXProgressElement} */ await fixture(template);
+
+            expect(component).lightDom.to.not.equal(template);
+        });
     });
 
-    it('should be instantiated with hx-defined attribute', async () => {
-        const el = /** @type { HXProgressElement } */ await fixture(template);
+    describe('test $onConnect method', () => {
+        
+        it('should have aria-valuemin attribute', async () => {
+            const component = /** @type {HXProgressElement} */ await fixture(template);
+            const attr = component.hasAttribute('aria-valuemin');
 
-        expect(el).to.have.attribute('hx-defined');
+            expect(attr).to.be.true;
+        });
+
+        it('should have aria-valuemax attribute', async () => {
+            const component = /** @type {HXProgressElement} */ await fixture(template);
+            const attr = component.hasAttribute('aria-valuemax');
+
+            expect(attr).to.be.true;
+        });
     });
 
-    it(`rendered Light DOM should NOT equal simple template ${template}`, async () => {
-        const el = /** @type { HXProgressElement } */ await fixture(template);
+    describe('test <hx-progress> getter and setter methods', () => {
+        it('should be able to pause element', async () => {
+            const component = /** @type {HXProgressElement} */ await fixture(template);
+            component.paused = true;
+            const attr = component.hasAttribute('value');
 
-        expect(el).lightDom.to.not.equal(template);
+            expect(attr).to.be.true;
+        });
     });
 
-    it('should have a static Shadow DOM', async function () {
-        const el = /** @type { HXProgressElement } */ await fixture(template);
-        const sd = el.shadowRoot.innerHTML; // FIXME: temp fix issue submitted https://github.com/open-wc/open-wc/issues/1540
-
-        expect(el).shadowDom.to.equal(sd);
-    });
 });
