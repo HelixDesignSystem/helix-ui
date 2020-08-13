438e9c9fd694cda5e6b1e12e39e2bebd88af2b66
BY00565233 <BY00565233@Techmahindra.com>
2020-08-13 18:02:50 +0530

    test(hx-checkbox-set): surf1999

---
+++ src/elements/hx-checkbox-set/index.spec.js
@@ -0,0 +1,101 @@
+import { fixture, oneEvent, expect } from '@open-wc/testing';
+
+/**
+ * <hx-checkbox-set> component tests
+ *
+ * @type HXCheckboxSetElement
+ *
+ */
+describe('<hx-checkbox-set> component tests', () => {
+    const template = '<hx-checkbox-set>';
+
+    describe('instantiate element', () => {
+        it('should be instantiated with hx-defined attribute', async () => {
+            const component = /** @type {HXCheckboxSetElement} */ await fixture(template);
+            const attr = component.hasAttribute('hx-defined');
+
+            expect(attr).to.be.true;
+        });
+
+        it('should not be hidden', async () => {
+            const component = /** @type {HXCheckboxSetElement} */ await fixture(template);
+            const prop = component.hidden;
+
+            expect(prop).to.be.false;
+        });
+
+        it(`the rendered light DOM should NOT equal simple template ${template}`, async () => {
+            const component = /** @type {HXCheckboxSetElement} */ await fixture(template);
+
+            expect(component).lightDom.to.not.equal(template);
+        });
+
+        it(`should NOT have a Shadow DOM`, async () => {
+            const component = /** @type {HXCheckboxSetElement} */ await fixture(template);
+            const shadow = component.shadowRoot;
+
+            expect(shadow).to.be.null;
+        });
+    });
+
+    describe('test <hx-checkbox-set> getters and setters', () => {
+        it(`should be able to get ${template} isDirty state`, async () => {
+            const component = /** @type {HXCheckboxSetElement} */ await fixture(template);
+            const attr = component.isDirty;
+
+            expect(attr).to.be.false;
+        });
+
+        it(`should be able to get ${template} wasChanged state`, async () => {
+            const component = /** @type {HXCheckboxSetElement} */ await fixture(template);
+            const attr = component.wasChanged;
+
+            expect(attr).to.be.false;
+        });
+
+        it(`should be able to get ${template} wasTouched state`, async () => {
+            const component = /** @type {HXCheckboxSetElement} */ await fixture(template);
+            const attr = component.wasTouched;
+
+            expect(attr).to.be.false;
+        });
+    });
+
+    describe('test for event listeners', () => {
+        it('should fire a change event', async () => {
+            const component = /** @type {HXCheckboxSetElement} */ await fixture(template);
+            const detail = { evt: 'hxchange!'};
+            const customEvent = new CustomEvent('hxchange', { detail });
+
+            setTimeout(() => component.dispatchEvent(customEvent));
+            const evt = await oneEvent(component, 'hxchange');
+
+            expect(evt).to.equal(customEvent);
+            expect(evt.detail).to.equal(detail);
+        });
+
+        it('should fire a dirty event', async () => {
+            const component = /** @type {HXCheckboxSetElement} */ await fixture(template);
+            const detail = { evt: 'hxdirty'};
+            const customEvent = new CustomEvent('hxdirty', { detail });
+
+            setTimeout(() => component.dispatchEvent(customEvent));
+            const evt = await oneEvent(component, 'hxdirty');
+
+            expect(evt).to.equal(customEvent);
+            expect(evt.detail).to.equal(detail);
+        });
+
+        it('should fire a touch event', async () => {
+            const component = /** @type {HXCheckboxSetElement} */ await fixture(template);
+            const detail = { evt: 'hxtouch'};
+            const customEvent = new CustomEvent('hxtouch', { detail });
+
+            setTimeout(() => component.dispatchEvent(customEvent));
+            const evt = await oneEvent(component, 'hxtouch');
+
+            expect(evt).to.equal(customEvent);
+            expect(evt.detail).to.equal(detail);
+        });
+    });
+});
