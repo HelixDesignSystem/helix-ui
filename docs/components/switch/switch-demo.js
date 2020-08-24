acacd8c626e9f30c3476eaf0c1a27007c0b96314
Lalith Kumar Karikelli <lalith.karikelli@rackspace.com>
2020-08-24 20:37:07 +0530

    refactor(hx-switch): documentation for switch

--- docs/components/switch/switch-demo.js
+++ docs/components/switch/switch-demo.js
@@ -1,28 +1,103 @@
 import Util from '../../_util';
 
-if (document.getElementById('vue-switchDemo')) {
-    new Vue({
-        el: '#vue-switchDemo',
-        data: {
-            isDisabled: false,
-            option : 'ON',
-            labelState: '',
-        },
-        computed: {
-            attrDisabled: function () {
-                return (this.isDisabled ? 'disabled' : '');
+(function () {
+    if (document.getElementById('vue-switchNoneDemo')) {
+        new Vue({
+            el: '#vue-switchNoneDemo',
+            computed: {
+                attrDisabled: function () {
+                    return (this.isDisabled ? 'disabled' : '');
+                },
+                snippet :function (){
+                    return Util.snippet(`
+                        <hx-switch-control>
+                          <input type="checkbox" id="switchNoneTest" />
+                          <label for="switchNoneTest">
+                            <hx-switch aria-labelledby="switchNoneTest"></hx-switch>
+                          </label>
+                        </hx-switch-control>
+                    `);
+                },
             },
-            snippet: function () {
-                return Util.snippet(`
-                    <hx-switch-control class="switch">
-                        <input type="checkbox" id="switchTest" ${this.isDisabled ? 'disabled' : ''} />
-                        <label for="switchTest">
-                            <hx-switch onlabel="on" offlabel="off" aria-labelledby="switchTest">
+        });
+    }
+    if (document.getElementById('vue-switchOnOffDemo')) {
+        new Vue({
+            el: '#vue-switchOnOffDemo',
+            computed: {
+                attrDisabled: function () {
+                    return (this.isDisabled ? 'disabled' : '');
+                },
+                snippet :function (){
+                    return Util.snippet(`
+                        <hx-switch-control>
+                          <input type="checkbox" id="switchOnOffTest" />
+                          <label for="switchOnOffTest">
+                            <hx-switch
+                              onlabel="on"
+                              offlabel="off"
+                              aria-labelledby="switchOnOffTest">
                             </hx-switch>
-                        </label>
-                    </hx-switch-control>
-                `);
+                          </label>
+                        </hx-switch-control>
+                    `);
+                },
             },
-        },
-    });
-}
+        });
+    }
+    if (document.getElementById('vue-switchYesNoDemo')) {
+        new Vue({
+            el: '#vue-switchYesNoDemo',
+            computed: {
+                attrDisabled: function () {
+                    return (this.isDisabled ? 'disabled' : '');
+                },
+                snippet :function (){
+                    return Util.snippet(`
+                        <hx-switch-control>
+                          <input type="checkbox" id="switchYesNoTest" />
+                          <label for="switchYesNoTest">
+                            <hx-switch
+                              onlabel="yes"
+                              offlabel="no"
+                              aria-labelledby="switchYesNoTest">
+                            </hx-switch>
+                          </label>
+                        </hx-switch-control>
+                    `);
+                },
+            },
+        });
+    }
+    if (document.getElementById('vue-switchDisabledDemo')) {
+        new Vue({
+            el: '#vue-switchDisabledDemo',
+            data: {
+                isDisabled: false,
+                option : 'ON',
+                labelState: '',
+            },
+            computed: {
+                attrDisabled: function () {
+                    return (this.isDisabled ? 'disabled' : '');
+                },
+                snippet: function () {
+                    return Util.snippet(`
+                        <hx-switch-control>
+                          <input
+                            type="checkbox"
+                            id="switchDisabledTest" ${this.isDisabled ? 'disabled' : ''} />
+                          <label for="switchDisabledTest">
+                            <hx-switch
+                              onlabel="on"
+                              offlabel="off"
+                              aria-labelledby="switchDisabledTest">
+                            </hx-switch>
+                          </label>
+                        </hx-switch-control>
+                    `);
+                },
+            },
+        });
+    }
+})();
--- src/elements/hx-switch-control/index.js
+++ src/elements/hx-switch-control/index.js
@@ -1,7 +1,7 @@
 import { HXFormControlElement } from '../../interfaces/HXFormControlElement/index.js';
 
 /**
- * Defines behavior for the `<hx-switch-control>` element which is the
+ * Defines behavior for the `<hx-switch-control>` element, which is the
  * controller for the `<hx-switch>` element.
  *
  * @extends HXFormControlElement
@@ -14,8 +14,21 @@ export class HXSwitchControlElement extends HXFormControlElement {
         return 'hx-switch-control';
     }
 
+    $onCreate () {
+        this._onClick = this._onClick.bind(this);
+    }
+
+    $onConnect () {
+        this.controlElement.addEventListener('click', this._onClick);
+    }
+
+    $onDisconnect () {
+        this.controlElement.removeEventListener('click', this._onClick);
+    }
+
     /**
-     * Fetch the first `<input type="checkbox">` descendant.
+     * Fetch the first `<input type="checkbox">`
+     * descendant (there should only be one(1)).
      *
      * @override
      * @readonly
@@ -23,5 +36,39 @@ export class HXSwitchControlElement extends HXFormControlElement {
      */
     get controlElement () {
         return this.querySelector('input[type="checkbox"]');
+    }
+
+    /**
+     * Get Switch Component state.
+     *
+     * @default false
+     * @type {Boolean}
+     */
+    get toggled () {
+        return this.switchElement.toggled;
+    }
+
+    /**
+     * Fetch the first `<hx-switch>` deccendant (there should only be one(1)).
+     *
+     * @private
+     */
+    get switchElement () {
+        return this.querySelector('hx-switch');
+    }
+
+    /** Determines the toggle state.
+     *
+     * @private
+     */
+    // eslint-disable-next-line no-unused-vars
+    _onClick (evt) {
+        let isChecked = this.controlElement.checked;
+
+        if (isChecked) {
+            this.switchElement.toggled = true;
+        } else {
+            this.switchElement.toggled = false;
+        }
     }
 }
