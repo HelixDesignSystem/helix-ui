db0a69de10af1587884f8204e78edff1b0676279
BY00565233 <BY00565233@Techmahindra.com>
2020-10-16 13:52:51 +0530

    fix(hx-beacon): to override the default beacon styles

--- docs/components/beacon/beacon-demo.js
+++ docs/components/beacon/beacon-demo.js
@@ -70,4 +70,49 @@ import Util from '../../_util';
             },
         });
     }//vue-beacon-inline-styles
+
+    if (document.getElementById('vue-beacon-Override-styles')) {
+        new Vue({
+            el: '#vue-beacon-Override-styles',
+            data: {
+                beaconOverrideStyles: 'top: 42%; left: 24px; --hxBeacon: #d32f2f;',
+                isOpen: false,
+                isDismissed: false,
+            },
+            methods: {
+                onEvent: function () {
+                    this.isOpen = true;
+                    this.isDismissed = true;
+                },
+
+            },
+            computed: {
+                beaconOverrideSnippet: function () {
+                    if (this.isDismissed) {
+                        return '';
+                    } else {
+                        return `<hx-beacon style="${this.beaconOverrideStyles}"></hx-beacon>`;
+                    }
+                },
+                snippetOverrideStyles: function () {
+                    return Util.snippet(`
+                        <!-- Providing a border around the <div> for illustration -->
+                        <div style="position: relative;">
+                          ${this.beaconOverrideSnippet}
+                          <hx-disclosure class="hxBtn" aria-controls="overrideMenuId">
+                            <hx-icon type="cog"></hx-icon>
+                            <span>Actions</span>
+                            <hx-icon class="hxPrimary" type="angle-down"></hx-icon>
+                          </hx-disclosure>
+                          <hx-menu id="overrideMenuId">
+                            <hx-menuitem>Action 1</hx-menuitem>
+                            <hx-menuitem>Action 2</hx-menuitem>
+                            <hx-menuitem>Action 3</hx-menuitem>
+                          </hx-menu>
+                        </div>
+                    `);
+                },
+            },
+        });
+    }//vue-beacon-Override-styles
 })();
