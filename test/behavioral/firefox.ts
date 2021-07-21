c217a663a4c5c8ab879b46a7eb70763c8a1a8738
100stacks <100stacks@users.noreply.github.com>
2021-07-21 14:39:57 -0500

    test(deprecate): remove legacy test structure

--- test/behavioral/firefox.ts
+++
@@ -1,3 +0,0 @@
-import * as behavioral from "./behavioral";
-
-behavioral.suite("firefox");
--- test/regression/generateSingleTest.js
+++
@@ -1,3 +0,0 @@
-module.exports = (browser, component, buildIdentifier) => {
-    return `import { regressionTest } from "../regression/runSingleTest"; regressionTest("${browser}", "${component}", "${buildIdentifier}");`
-}
